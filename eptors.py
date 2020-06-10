b623e86644cdb07fdf5c8db6f8bde20c0555c3a0
AstronDog <junwangucas@gmail.com>
2020-06-10 17:46:27 +0200

    change the plots and n_toas

--- eptors.py
+++ eptors.py
@@ -24,22 +24,14 @@ import os, argparse, subprocess
 def res_obtain(obs, eph):
     os.system('''cat %s | awk '{if ($1 != "C" && $1 != "MODE") print $0}' > %s_tmp'''%(obs,obs))
 
-    # count=len(open(r"train.data",'rU').readlines())
-    n_toas = len(open(obs,'rU').readlines())
-    os.environ['var']=str(n_toas)
-  
-
-    os.system("tempo2 -output general2 -f %s %s_tmp -npsr 1 -nobs $n_toas -s \
-              \"tadan {freq} {post} {err}e-6\n\" | grep 'tadan'| awk \'{print $2,$3,$4}\' \
-              > tmp.dat"%(eph,obs))
+    os.system('''tempo2 -output general2 -s "tadan {freq} {post} {err}e-6\n" -f %s %s_tmp -npsr 1 \
+              -nobs 50000 | grep 'tadan'| awk '{print $2,$3,$4}' > tmp.dat'''%(eph,obs))
     freq, post, err = np.genfromtxt("./tmp.dat")[:,0], np.genfromtxt("./tmp.dat")[:,1], np.genfromtxt("./tmp.dat")[:,2]
     freq = freq.reshape(-1,1)
     return freq, post, err
 
 def show_residual(obs, eph):
-    n_toas = len(open(obs,'rU').readlines())
-    os.environ['var']=str(n_toas)
-    subprocess.call('tempo2 -gr plk -f %s %s -npsr 1 -nobs $n_toas -showchisq'%(eph, obs), shell=True)
+    subprocess.call('tempo2 -gr plk -f %s %s -npsr 1 -nobs 50000 -showchisq'%(eph, obs), shell=True)
 
 
 def trim(obs,eph):
@@ -56,14 +48,10 @@ def trim(obs,eph):
     in_mask = (residuals > median - 3*MAD) & (residuals < median + 3*MAD)
     out_mask = (residuals <= median - 3*MAD) | (residuals >= median + 3*MAD)   
 
-    trim_del_toa = post[out_mask]
-    trim_del_err = err[out_mask]
-    trim_del_freq= freq[out_mask]
-    
-    trim_keep_freq = freq[in_mask]
-    trim_keep_toa = post[in_mask]
-    trim_keep_err = err[in_mask]
-    
+
+    trim_del_toa,trim_del_err,trim_del_freq = post[out_mask], err[out_mask], freq[out_mask]    
+    trim_keep_freq,trim_keep_toa,trim_keep_err = freq[in_mask],post[in_mask],err[in_mask]
+ 
 
     df = pd.read_csv("%s_tmp"%(obs), skiprows=1, dtype=str, header=None, delim_whitespace=True)
 
@@ -74,31 +62,31 @@ def trim(obs,eph):
     df.to_csv("trim.tim", sep=' ', header=False, index=False)
     os.system("sed -i '1iFORMAT 1' trim.tim")
     #Remove mediators
-    os.system("rm tmp.dat")
-    os.system("rm %s_tmp"%(obs))
+    os.system("rm tmp.dat  %s_tmp" %(obs))
     
     return trim_del_toa, trim_del_err, trim_del_freq, trim_keep_toa, trim_keep_err, trim_keep_freq
 
 def trim_plot(obs, eph):    
     freq, post, err = res_obtain(obs, eph)
-    trim_del_toa, trim_del_err,trim_del_freq,_,_,_ = trim(obs,eph)
-
-
-    #Plot the orignal residuals
-    plt.errorbar(freq,post,yerr=err,fmt='b.')
-    
+    trim_del_toa, trim_del_err, trim_del_freq, trim_keep_toa, trim_keep_err, trim_keep_freq = trim(obs,eph)
+   
     # PLot the fitted data with Huber regression model
     model = make_pipeline(PolynomialFeatures(2), HuberRegressor())
     model.fit(freq, post, huberregressor__sample_weight=np.ravel(1/err))
     x_plot = np.linspace(freq.min(), freq.max())
     y_plot = model.predict(x_plot[:, np.newaxis])
-    plt.plot(x_plot,y_plot,'r-')
+    plt.plot(x_plot,y_plot,'k-')
     
-    #Plot the inlier residuals
-    plt.errorbar(trim_del_freq, trim_del_toa, yerr=trim_del_err,fmt='k.')
+    #Plot the outlier residuals
+    plt.errorbar(trim_del_freq, trim_del_toa, yerr=trim_del_err,fmt='r.', label="Outliers")
+
+    #Plot the Inliers
+    plt.errorbar(trim_keep_freq,trim_keep_toa,yerr=trim_keep_err,fmt='b.',label="Inliers")
+    
+    plt.title("Median absolute deviation method")
     plt.xlabel('Frequency (MHz)')
     plt.ylabel('Post-fit Residuals (sec)')
-    
+    plt.legend()
     plt.show()
 
 def gauss_fit(obs, eph):       
@@ -115,9 +103,9 @@ def gauss_fit(obs, eph):
     x = [0.5 * (data[1][i] + data[1][i+1]) for i in np.arange(len(data[1])-1)]
     y = data[0]
 
-    ar = np.trapz(y/np.sum(y)*len(y), x)
-    mean = np.mean(x * y / np.sum(y) * len(y))
-    rms = np.std(x * y / np.sum(y) * len(y))
+
+
+    ar,mean,rms = np.trapz(y/np.sum(y)*len(y), x),np.mean(x * y / np.sum(y) * len(y)),np.std(x * y / np.sum(y) * len(y))
     popt, pcov = curve_fit(f, x, y, p0=[ar, mean, rms])
     
 
@@ -125,15 +113,8 @@ def gauss_fit(obs, eph):
     in_mask =  (rvu > popt[1] - 4 * abs(popt[2])) & (rvu < popt[1] + 3 * abs(popt[2]))
     out_mask =  (rvu <= popt[1] - 4 * abs(popt[2])) | (rvu >= popt[1] + 3 * abs(popt[2]))
     
-
-    gaus_keep_toa = post[in_mask]
-    gaus_keep_err = err[in_mask]
-    
-    gaus_del_toa = post[out_mask]
-    gaus_del_err = err[out_mask]
-    gaus_del_freq = freq[out_mask]
-    
-
+    gaus_keep_toa,gaus_keep_err,gaus_keep_freq = post[in_mask],err[in_mask],freq[in_mask]   
+    gaus_del_toa,gaus_del_err,gaus_del_freq = post[out_mask],err[out_mask],freq[out_mask]
    
 
     df = pd.read_csv("%s_tmp"%(obs), skiprows=1, dtype=str, header=None, delim_whitespace=True)
@@ -147,19 +128,21 @@ def gauss_fit(obs, eph):
        
     os.system("rm %s_tmp"%(obs))
     plt.close()
-    return gaus_del_freq, gaus_del_toa, gaus_del_err, gaus_keep_toa, gaus_keep_err, 
+    return gaus_del_freq, gaus_del_toa, gaus_del_err, gaus_keep_toa, gaus_keep_err, gaus_keep_freq
 
 
 def gauss_plot(obs, eph):
     freq, post, err = res_obtain("trim.tim", eph)
-    gaus_del_freq, gaus_del_toa, gaus_del_err, _, _  =  gauss_fit("trim.tim", eph)    
+    gaus_del_freq, gaus_del_toa, gaus_del_err, gaus_keep_toa, gaus_keep_err, gaus_keep_freq  =  gauss_fit("trim.tim", eph)    
     
-    plt.errorbar(freq,post,yerr=err,fmt='k.')    
-    plt.errorbar(gaus_del_freq, gaus_del_toa, yerr=gaus_del_err,fmt='c.')
-    
-    plt.ylabel('Timing residual / ToA uncertainty')
-    plt.show()
 
+    plt.errorbar(gaus_keep_freq,gaus_keep_toa,yerr=gaus_keep_err,fmt='b.',label="Inliers")
+    plt.errorbar(gaus_del_freq, gaus_del_toa, yerr=gaus_del_err,fmt='c.', label="Outliers")
+    
+    plt.title("Gaussian distribution fitting method")
+    plt.ylabel('Timing residual / ToA uncertainty')
+    plt.legend()
+    plt.show()
 
     
 def showeffect(obs, eph, algo): 
@@ -173,15 +156,15 @@ def showeffect(obs, eph, algo):
 
         
     if algo == "t":   
-        plt.errorbar(np.arange(0, len(trim_keep_toa), 1 ), trim_keep_toa , yerr=trim_keep_err, fmt='b.', label='Inliers')
+        plt.errorbar(np.arange(0, len(trim_keep_toa), 1), trim_keep_toa, yerr=trim_keep_err, fmt='b.', label='Inliers')
     
         plt.errorbar(np.arange(len(trim_keep_toa), len(trim_keep_toa) + len(trim_del_toa), 1), 
                                trim_del_toa, yerr=trim_del_err, fmt='r.', label='Trim-removed outliers')
         plt.xlim((-4, len(trim_del_toa) + len(trim_keep_toa) + 4)) 
     else:
-        _,gaus_del_toa, gaus_del_err, gaus_keep_toa, gaus_keep_err = gauss_fit("trim.tim", eph)
+        _,gaus_del_toa, gaus_del_err, gaus_keep_toa, gaus_keep_err,_ = gauss_fit("trim.tim", eph)
         
-        plt.errorbar(np.arange(0, len(gaus_keep_toa), 1 ), gaus_keep_toa, yerr=gaus_keep_err, fmt='b.', label='Inliers')
+        plt.errorbar(np.arange(0, len(gaus_keep_toa), 1), gaus_keep_toa, yerr=gaus_keep_err, fmt='b.', label='Inliers')
         
         plt.errorbar(np.arange(len(gaus_keep_toa), len(gaus_keep_toa) + len(trim_del_toa), 1), 
                                trim_del_toa, yerr=trim_del_err, fmt='r.', label='Trim-removed outliers')
@@ -192,9 +175,7 @@ def showeffect(obs, eph, algo):
         
         plt.xlim((-4, len(gaus_keep_toa) + len(trim_del_toa) + len(gaus_del_toa) + 4))
         
-        
-
-            
+    plt.title("Outlier removed by each method")
     plt.legend(loc='upper left')
     plt.ylabel('Post-fit Residual (sec)')
     plt.xlabel('TOA number')
@@ -202,11 +183,7 @@ def showeffect(obs, eph, algo):
 
     ax = plt.gca() 
     plt.ticklabel_format(axis='y', style='sci') 
-    ax.yaxis.major.formatter.set_powerlimits((0,0)) 
-
-
-    
-    
+    ax.yaxis.major.formatter.set_powerlimits((0,0))    
     plt.show()
 
 
